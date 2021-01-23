8f0e5b6ba771c32134f5f08fa3ebad767d545648
Nikolas Dahn <nikolas.dahn@gmail.com>
2021-01-23 14:02:43 +0100

    Adjusted panel opening/switching for small devices (<768px wide)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import Split from 'split.js'
 
 // Split panes
 Split(['#split-pane-1', '#split-pane-2'], {
+    minSize: 0,
+    snapOffset: 20,
     gutterStyle: function (dim, size, index) {
         // return an empty style, we do everything in CSS
         return {};
@@ -11,27 +13,38 @@ Split(['#split-pane-1', '#split-pane-2'], {
 
 // Tabs
 let animDuration = 200;
+let targetRatio = 50;
+if (document.body.clientWidth < 768) {
+    targetRatio = 100;
+    $('#split-pane-2').hide();
+    $('#split-pane-1').css('width', '100%');
+}
 
 $('#blocks-btn').on('click', evt => {
     let view = $('#split-pane-1');
+    view.show();
     if (view.width() > 0) {
+        // Shrink blocks view to 0, expand 3D view
         view.animate({ width: 0 }, animDuration);
         $('#split-pane-2').animate({ width: '100%' }, animDuration);
     } else {
-        // TODO if (window.matchMedia('(max-width: '))
-        view.animate({ width: '50%' }, animDuration);
-        $('#split-pane-2').animate({ width: '50%' }, animDuration);
+        // Expand blocks view to targetRatio, 3D view will take up the rest
+        view.animate({ width: targetRatio + '%' }, animDuration);
+        $('#split-pane-2').animate({ width: (100 - targetRatio) + '%' }, animDuration);
     }
 });
 
 $('#viewport-btn').on('click', evt => {
     let view = $('#split-pane-2');
+    view.show();
     if (view.width() > 0) {
+        // Shrink 3D view to 0, expand blocks view
         view.animate({ width: 0 }, animDuration);
         $('#split-pane-1').animate({ width: '100%' }, animDuration);
     } else {
-        view.animate({ width: '50%' }, animDuration);
-        $('#split-pane-1').animate({ width: '50%' }, animDuration);
+        // Expand 3D view to targetRatio, blocks view will take up the rest
+        view.animate({ width: targetRatio + '%' }, animDuration);
+        $('#split-pane-1').animate({ width: (100 - targetRatio) + '%' }, animDuration);
     }
 });
 
