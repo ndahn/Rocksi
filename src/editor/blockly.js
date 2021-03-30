09e8791c8ff9f6ce302559d41ba41a12afde1fff
lgreipel <fischthun@localhost.localdomain>
2021-03-30 18:33:56 +0200

    You can now add and remove blockly blocks to create 3D-objects. The 3D-object can be change into a cylinder. Some clean up

--- src/editor/blockly.js
+++ src/editor/blockly.js
@@ -25,6 +25,9 @@ import './blocks/joint_unlock'
 //points to block definition for the add3dbox, Lukas
 import './blocks/addSimObject'
 
+//imports for adding and removing 3D-objects
+import {addSimObjects, remSimObjects, getSimObjectsIds} from '../simulator/objects/create3dObject'
+
 import { popSuccess, popWarning, popError } from '../alert'
 
 const generator = Blockly.JavaScript;
@@ -362,50 +365,40 @@ function onProgramFinished() {
     popSuccess(Blockly.Msg['EXEC_SUCCESS'] || "Program finished");
 }
 
-//experimenting with the eventListener, Lukas
-
-let blocksIdList = [];
-let simObjects = [];
-
+//Determin if a addSimObject-block was added or removed form the Blockly Workspace.
+//If added, add a new 3D-object. If removed remove the 3D-object assosiated with the block.
+//Lukas
 function watchBlocks(event) {
 
+    //get all ids of currently rendered 3D-block representations
+    let simObjectsIds = getSimObjectsIds();
+    //if there is a new block added to the workspace store it
     const newBlock = workspace.getBlockById(event.blockId);
-    let currentBlocksList = workspace.getAllBlocks();
-    let currentBlocksIdList = [];
 
-    if (event.type === Blockly.Events.BLOCK_CREATE && newBlock != null){
+    let deletedSimObjects = [];
+    let currentSimObjects = [];
+    let currentSimObjectIds = [];
 
-        blocksIdList.push(newBlock.id);
+    currentSimObjects = workspace.getBlocksByType('addSimObject');
+    //put all blocks ids that are currently on the workspace into an array
+    currentSimObjects.forEach(block => currentSimObjectIds.push(block.id));
+    //clear memory
+    currentSimObjects = [];
 
-        if (newBlock.type === 'addSimObject'){
-            //update list with 3D objects
-            //and tell the create3dObject script
-            simObjects.push(newBlock.id);
-            console.log('ID', newBlock.id);
-            console.warn('Updated 3D objects list. Waring not implemented');
-            //update3dobjects(simObjects);
-        }
+    if (event.type === Blockly.Events.BLOCK_CREATE
+        && newBlock != null
+        && newBlock.type === 'addSimObject'){
+
+        console.log('newBlock.id', newBlock.id);
+        addSimObjects([newBlock.id]);
     }
 
     if (event.type === Blockly.Events.BLOCK_DELETE) {
 
-        currentBlocksList.forEach(block => currentBlocksIdList.push(block.id));
-        currentBlocksList = [];
+        deletedSimObjects = [...simObjectsIds].filter(blockId => !currentSimObjectIds.includes(blockId));
 
-        let compareBlockList = new Set(currentBlocksIdList);
-        let deletedBlocks = [...blocksIdList].filter(blockId => !compareBlockList.has(blockId));
-
-        blocksIdList = currentBlocksIdList;
-        currentBlocksIdList = [];
-
-        let deletedBlocksSet = new Set(deletedBlocks);
-        let deletedSimObjects = [...simObjects].filter(id => deletedBlocksSet.has(id));
-
-        simObjects = simObjects.filter(id => !deletedSimObjects.includes(id))
-
+        remSimObjects(deletedSimObjects);
         deletedSimObjects = [];
-        deletedBlocks = [];
-        console.warn('Updated 3D objects list. Waring not implemented');
     }
 }
 
