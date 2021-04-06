2114ecf1630963596da909df5626bec5d40a6d3d
lgreipel <fischthun@localhost.localdomain>
2021-04-06 10:38:22 +0200

    You can now add and move an object via the blockly block.

--- src/editor/blocks/addSimObject.js
+++ src/editor/blocks/addSimObject.js
@@ -1,6 +1,8 @@
 import * as Blockly from 'blockly';
-//create3dObject
-import {changeSimObject, simObject, getSimObject} from "../../simulator/objects/create3dObject";
+import {changeSimObjectType,
+        changeSimObjectPosition,
+        changeSimObjectOrientation,
+        getSimObject} from "../../simulator/objects/objects";
 
 
 Blockly.Blocks['addSimObject'] = {
@@ -8,7 +10,7 @@ Blockly.Blocks['addSimObject'] = {
         var thisBlock = this;
 		this.jsonInit({
 			"type": "addObject",
-			"message0": "Objekt: Typ: %1 x: %2 y: %3 z: %4 rot z: %5",
+			"message0": "Objekt: Typ: %1 x: %2 y: %3 z: %4 rot x: %5 rot y: %6 rot z: %7",
 			"args0": [
 			  {
 				"type": "field_dropdown",
@@ -43,17 +45,34 @@ Blockly.Blocks['addSimObject'] = {
             {
               "type": "field_number",
               "name": "POSITION_Z",
-              "value": 5,
-              "min": -10,
+              "value": 0,
+              "min": 0,
               "max": 10,
-              "precision": 0.01            },
+              "precision": 0.01
+            },
+            {
+              "type": "field_angle",
+              "name": "ROT_X",
+              "value": 0,
+              "min": 0,
+              "max": 360,
+              "precision": 1
+            },
+            {
+              "type": "field_angle",
+              "name": "ROT_Y",
+              "value": 0,
+              "min": 0,
+              "max": 360,
+              "precision": 1
+            },
             {
               "type": "field_angle",
               "name": "ROT_Z",
               "value": 0,
-              "min": 0.,
+              "min": 0,
               "max": 360,
-              "precision": 0.01
+              "precision": 1
             }
 			],
 			"colour": "",
@@ -62,40 +81,57 @@ Blockly.Blocks['addSimObject'] = {
 		  });
 	},
 	onchange: function (event) {
-        //this is it... IDEA: I need to tell create3dObject thisBlock.id
         var thisBlock = this;
-        var thisSimObject = new simObject;
-        thisSimObject.name = thisBlock.id;
-        thisSimObject = getSimObject(thisSimObject)
-        console.log('thisSimObject.name before switch', thisSimObject)
-		if (event.name === 'OBJECT_TYPE') {
-            console.log()
+		if (event.name === 'OBJECT_TYPE' && event.blockId == thisBlock.id) {
 			switch (event.newValue) {
-				case 'cube':
-                    thisSimObject.type = 'cube';
-                    changeSimObject(thisSimObject);
+    			case 'cube':
+                    console.log('You are changing Block.id: ', thisBlock.id);
+                    changeSimObjectType(thisBlock.id, 'cube');
 					break;
                 case 'cylinder':
-                    thisSimObject.type = 'cylinder';
-                    console.log('thisSimObject cylinder switch', thisSimObject)
-                    changeSimObject(thisSimObject);
+                    console.log('You are changing Block.id: ', thisBlock.id);
+                    changeSimObjectType(thisBlock.id, 'cylinder');
                     break;
                 default:
     				console.error('Error: ');
 			}
 		}
-        if (event.name === 'POSITION_X') {
-
+        if (event.name === 'POSITION_X' && event.blockId == thisBlock.id) {
+            var simObject = getSimObject(thisBlock.id);
+            simObject.x = thisBlock.getFieldValue('POSITION_X');
+            changeSimObjectPosition(simObject);
         }
-        if (event.name === 'POSITION_Y') {
-
+        if (event.name === 'POSITION_Y' && event.blockId == thisBlock.id) {
+            var simObject = getSimObject(thisBlock.id);
+            simObject.y = thisBlock.getFieldValue('POSITION_Y');
+            changeSimObjectPosition(simObject);
         }
-        if (event.name === 'POSITION_Z') {
-
+        if (event.name === 'POSITION_Z' && event.blockId == thisBlock.id) {
+            var simObject = getSimObject(thisBlock.id);
+            simObject.z = thisBlock.getFieldValue('POSITION_Z');
+            changeSimObjectPosition(simObject);
         }
-        if (event.name === 'ROT_Z') {
-
+        if (event.name === 'ROT_X' && event.blockId == thisBlock.id) {
+            var simObject = getSimObject(thisBlock.id);
+            simObject.rotX = thisBlock.getFieldValue('ROT_X');
+            changeSimObjectOrientation(simObject);
+        }
+        if (event.name === 'ROT_Y' && event.blockId == thisBlock.id) {
+            var simObject = getSimObject(thisBlock.id);
+            simObject.rotY = thisBlock.getFieldValue('ROT_Y');
+            changeSimObjectOrientation(simObject);
+        }
+        if (event.name === 'ROT_Z' && event.blockId == thisBlock.id) {
+            var simObject = getSimObject(thisBlock.id);
+            simObject.rotZ = thisBlock.getFieldValue('ROT_Z');
+            changeSimObjectOrientation(simObject);
         }
 
 	},
+};
+
+//not working right now.
+Blockly.JavaScript["addSimObject"] = function (block) {
+    var code = '';
+	return code;
 };
