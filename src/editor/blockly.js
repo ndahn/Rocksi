aba8280c61a7283a6c461d4bdf5301e487af96da
lgreipel <lukas.greipel@gmail.com>
2021-05-27 16:42:23 +0200

    Blockly field values are now given to the addSimObject function.

--- src/editor/blockly.js
+++ src/editor/blockly.js
@@ -391,7 +391,6 @@ function watchSpawnBlocks(event) {
                         pickedColour = undefined;
                     }
                 }
-                console.log(pickedColour);
                 addSimObject(newBlock.id, fieldValues, pickedColour);
             }
         }
