b3277e154e6e38a820d44b9c83a14d48658a6c74
lgreipel <lukas.greipel@gmail.com>
2021-06-01 15:58:48 +0200

    Block is true when the physics sim is finished.

--- src/editor/blocks/physics_done.js
+++ src/editor/blocks/physics_done.js
@@ -6,11 +6,11 @@ Blockly.Blocks["physics_done"] = {
 	init: function () {
 		this.jsonInit({
 			type: "physics_done",
-			message0: "Läuft die Physiksimulation?",
+			message0: "Ist die Physiksimulation abgeschlossen?",
 			output: "Boolean",
 			style: 'objects_blocks',
 			tooltip:
-				"Gibt Wahr zurück wenn die Physiksimulation läuft.",
+				"Ist die Physiksimulation abgeschlossen?",
 			helpUrl: "",
 		});
     }
@@ -19,8 +19,8 @@ Blockly.Blocks["physics_done"] = {
 Blockly.JavaScript["physics_done"] = function (block) {
     let physicsDone;
     Simulation.getInstance(sim => {
-        physicsDone = !sim.getPhysicsDone();
-    });
+        physicsDone = sim.getPhysicsDone();
+    }); //simulation.instance benutzen
     let ret = '["physics_done", ' + physicsDone + ']';
     console.log(ret);
     return [ret, Blockly.JavaScript.ORDER_ATOMIC];
